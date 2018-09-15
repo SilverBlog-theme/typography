fe608ef33fecf1870cb0ca89784f8160c0029674
tcdw <tcdw2011@gmail.com>
2018-09-15 11:37:05 +0800

    完成编译

--- scripts/build-sass.js
+++ scripts/build-sass.js
@@ -1,8 +1,17 @@
-const compileSass = require('compile-sass');
+const sass = require('node-sass');
+const CleanCSS = require('clean-css');
+const fs = require('fs');
 const path = require('path');
 
-compileSass.compileSassAndSaveMultiple({
-    sassPath: path.resolve(__dirname, '../source/scss/'),
-    cssPath: path.resolve(__dirname, '../static/css/'),
-    files: ['style.scss', 'style-dark.scss'],
-});
+let style = sass.renderSync({
+    file: path.resolve(__dirname, '../source/scss/style.scss'),
+}).css.toString('utf8');
+let styleDark = sass.renderSync({
+    file: path.resolve(__dirname, '../source/scss/style-dark.scss'),
+}).css.toString('utf8');
+
+style = new CleanCSS().minify(style).styles;
+styleDark = new CleanCSS().minify(styleDark).styles;
+
+fs.writeFileSync(path.resolve(__dirname, '../static/css/style.css'), style, { encoding: 'utf8' });
+fs.writeFileSync(path.resolve(__dirname, '../static/css/style-dark.css'), styleDark, { encoding: 'utf8' });
